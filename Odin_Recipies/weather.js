882c525d02819f34da4a2cc962462913cbae1e7d
Tylergm924 <amanace924@gmail.com>
2023-01-18 18:53:12 +0300

    magic8ball

--- Odin_Recipies/weather.js
+++
@@ -1,8 +0,0 @@
-let kelvin = 293 // the weather
-let celsius = kelvin -273 // convert weather from kelvin to celsius
-let fahrenheit = celsius * (9/5)+32 //convert weather from celsius to fahrenheit
-fahrenheit = Math.round(fahrenheit) //rounding fahrenheit value to non decimal number
-let newton = celsius * (33/100) //convert celsius to newton scale
-newton = Math.round(newton)
-console.log("The temprature is "+fahrenheit+" degrees Fahrenheit")
-console.log("The temprature is "+newton+" degrees Newton")
